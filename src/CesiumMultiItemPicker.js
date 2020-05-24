0d791085e04621cd525f8fa4f3f0a40731c16d61
Chris <chris.mcleese86@gmail.com>
2020-05-24 13:19:11 +0200

    initial commit

---
+++ src/CesiumMultiItemPicker.js
@@ -0,0 +1,124 @@
+import {
+  defined,
+  DeveloperError,
+  ScreenSpaceEventHandler,
+  ScreenSpaceEventType,
+  Event
+} from 'cesium'
+
+export default class CesiumMultiItemPicker {
+  constructor (viewer) {
+    if (!defined(viewer)) {
+      throw new DeveloperError('viewer is required.')
+    }
+    // set references
+    // the viewer instance
+    this._viewer = viewer
+    // picked items list
+    this.pickedList = []
+
+    /**
+     * The event raised when an entity or imagery layer is selected.
+     *
+     * @return  {Array}  The list of selected items.
+     */
+    this.onPicked = new Event()
+
+    // initialize the handler
+    this.init()
+  }
+
+  /**
+   * Initialize the mouse handler events and picker.
+   */
+  init () {
+    // create mouse handler
+    this.mouseHandler = new ScreenSpaceEventHandler(this._viewer.scene.canvas)
+    // create the callback for mouse left up
+    this.mouseHandler.setInputAction(
+      mousePicker.bind(this),
+      ScreenSpaceEventType.LEFT_UP
+    )
+  }
+
+  /**
+   * Destory the mouse handler.
+   */
+  destroy () {
+    this.mouseHandler && this.mouseHandler.destroy()
+  }
+}
+
+/**
+ * The method called when a position is selected from the mouse event.
+ *
+ * @param   {Object}  mouse  The mouse position object (Cartesian2).
+ *
+ * @return  {Boolean|Event}  The event raised or cancelled event.
+ */
+async function mousePicker (mouse) {
+  const mousePos = mouse.position
+  // if the position contains Cartesian3 coordiantes then the position is on the globe.
+  if (!isCartesian.call(this, mousePos)) {
+    return false
+  }
+  // reset the picked list
+  this.pickedList = []
+  // if there are any enabled imagery layers aside from the basemap layer
+  if (this._viewer.scene.imageryLayers.length > 1) {
+    // get any possible imagery layer features at the provided position coordinates
+    const imageryLayerFeatures = await getImageryLayerFeatures.call(this, mousePos)
+    // if imagery layer features are present
+    if (imageryLayerFeatures.length) {
+      // store the imagery layers feature info
+      this.pickedList = this.pickedList.concat(imageryLayerFeatures)
+    }
+  }
+
+  // look for any enties at the provided position coordinates
+  const pickedEntities = this._viewer.scene.drillPick(mousePos)
+  // if there are entities found
+  if (pickedEntities.length) {
+    // store the entities
+    this.pickedList = this.pickedList.concat(pickedEntities)
+  }
+  // if there items picked and found, raise the event
+  return this.pickedList.length && this.onPicked.raiseEvent(this.pickedList)
+}
+
+/**
+ * Get imagery layer feature info.
+ *
+ * @param   {Cartesian2}  position  The position for lookup.
+ *
+ * @return  {Array}                 The array of features.
+ */
+async function getImageryLayerFeatures (position) {
+  // get the ray for lookup
+  const pickRay = this._viewer.camera.getPickRay(position)
+  // look for imagery layer features at provided ray
+  const featuresPromise = await this._viewer.imageryLayers.pickImageryLayerFeatures(
+    pickRay,
+    this._viewer.scene
+  )
+  // filter any found features that only include feature info properties
+  const filteredFeatures = featuresPromise.filter(function (x) {
+    return x.properties && Object.getOwnPropertyNames(x.properties).length
+  })
+  // return the filtered features
+  return filteredFeatures
+}
+
+/**
+ * Check to see if the provided position is in fact on the ellipsoid (globe).
+ *
+ * @param   {Cartesian2}  position  The position for verification.
+ *
+ * @return  {Cartesian3}            The found position on the ellipsoid.
+ */
+function isCartesian (position) {
+  return this._viewer.camera.pickEllipsoid(
+    position,
+    this._viewer.scene.globe.ellipsoid
+  )
+}
---
+++ src/viewerCesiumMultiItemPickerMixin.js
@@ -0,0 +1,47 @@
+import { defined, DeveloperError, wrapFunction } from 'cesium'
+import CesiumMultiItemPicker from './CesiumMultiItemPicker'
+/**
+ * A mixin which adds deeper ellipsoid position picking capabilities.
+ * The viewer should be extended to include this mixin.
+ *
+ * @exports viewerCesiumMultiItemPickerMixin
+ *
+ * @param   {Viewer}  viewer   The viewer instance.
+ *
+ * @exception {DeveloperError} viewer is required.
+ * @exception {DeveloperError} cesiumMultiItemPicker is already defined by another mixin.
+ *
+ * @example
+ * // Add multi item picker support to viewer.
+ * var viewer = new Cesium.Viewer('cesiumContainer')
+ * viewer.extend(cesiumMultiItemPicker)
+ * viewer.cesiumMultiItemPicker.onPicked.addEventListener((picked) => {
+ *   console.log(picked)
+ * , this)
+ */
+function viewerCesiumMultiItemPickerMixin (viewer) {
+  if (!defined(viewer)) {
+    throw new DeveloperError('viewer is required.')
+  }
+  if (Object.prototype.hasOwnProperty.call(viewer, 'cesiumMultiItemPicker')) {
+    throw new DeveloperError('cesiumMultiItemPicker is already defined by another mixin.')
+  }
+
+  const cesiumMultiItemPicker = new CesiumMultiItemPicker(
+    viewer
+  )
+
+  Object.defineProperties(viewer, {
+    cesiumMultiItemPicker: {
+      get: function () {
+        return cesiumMultiItemPicker
+      }
+    }
+  })
+
+  viewer.destroy = wrapFunction(viewer, viewer.destroy, function () {
+    viewer.cesiumMultiItemPicker.destroy()
+  })
+}
+
+export default viewerCesiumMultiItemPickerMixin
