1fd5aa71f2723dc6e3e5bc3450469fd13929e8dd
Chris <chris.mcleese86@gmail.com>
2020-05-27 20:20:16 +0200

    check for click and drag movement before doing map pick

--- src/CesiumMultiItemPicker.js
+++ src/CesiumMultiItemPicker.js
@@ -3,11 +3,12 @@ import {
   DeveloperError,
   ScreenSpaceEventHandler,
   ScreenSpaceEventType,
-  Event
+  Event,
+  Cartesian2
 } from 'cesium'
 
 export default class CesiumMultiItemPicker {
-  constructor (viewer) {
+  constructor(viewer) {
     if (!defined(viewer)) {
       throw new DeveloperError('viewer is required.')
     }
@@ -16,13 +17,10 @@ export default class CesiumMultiItemPicker {
     this._viewer = viewer
     // picked items list
     this.pickedList = []
-
-    /**
-     * The event raised when an entity or imagery layer is selected.
-     *
-     * @return  {Array}  The list of selected items.
-     */
+    // The event raised when an entity or imagery layer is selected.
     this.onPicked = new Event()
+    // track the last mouse down position
+    this.lastMouseDownPos = Cartesian2.ZERO
 
     // initialize the handler
     this.init()
@@ -31,9 +29,19 @@ export default class CesiumMultiItemPicker {
   /**
    * Initialize the mouse handler events and picker.
    */
-  init () {
+  init() {
     // create mouse handler
     this.mouseHandler = new ScreenSpaceEventHandler(this._viewer.scene.canvas)
+
+    // create the callback for mouse left down
+    this.mouseHandler.setInputAction(
+      // track the last mouse down position
+      mouse => {
+        this.lastMouseDownPos = mouse.position
+      },
+      ScreenSpaceEventType.LEFT_DOWN
+    )
+
     // create the callback for mouse left up
     this.mouseHandler.setInputAction(
       mousePicker.bind(this),
@@ -44,7 +52,7 @@ export default class CesiumMultiItemPicker {
   /**
    * Destory the mouse handler.
    */
-  destroy () {
+  destroy() {
     this.mouseHandler && this.mouseHandler.destroy()
   }
 }
@@ -57,35 +65,37 @@ export default class CesiumMultiItemPicker {
  * @return  {Boolean|Event}  The event raised or cancelled event.
  *                           Args passed is array of: [{Entity}, {ImageryLayerFeatureInfo}]
  */
-async function mousePicker (mouse) {
+async function mousePicker(mouse) {
   const mousePos = mouse.position
-  // if the position contains Cartesian3 coordiantes then the position is on the globe.
-  if (!isCartesian.call(this, mousePos)) {
-    return false
-  }
-  // reset the picked list
-  this.pickedList = []
-  // if there are any enabled imagery layers aside from the basemap layer
-  if (this._viewer.scene.imageryLayers.length > 1) {
-    // get any possible imagery layer features at the provided position coordinates
-    const imageryLayerFeatures = await getImageryLayerFeatures.call(this, mousePos)
-    // if imagery layer features are present
-    if (imageryLayerFeatures.length) {
-      // store the imagery layers feature info
-      this.pickedList = this.pickedList.concat(imageryLayerFeatures)
+  // if the current position contains Cartesian coordiantes then the position is on the globe
+  // and if the last mouse down position matches the mouse up position (not clicking and dragging)
+  if (this.lastMouseDownPos.equals(mousePos) && isCartesian.call(this, mousePos)) {
+    // reset the picked list
+    this.pickedList = []
+    // if there are any enabled imagery layers aside from the basemap layer
+    if (this._viewer.scene.imageryLayers.length > 1) {
+      // get any possible imagery layer features at the provided position coordinates
+      const imageryLayerFeatures = await getImageryLayerFeatures.call(this, mousePos)
+      // if imagery layer features are present
+      if (imageryLayerFeatures.length) {
+        // store the imagery layers feature info
+        this.pickedList = this.pickedList.concat(imageryLayerFeatures)
+      }
     }
-  }
 
-  // look for any enties at the provided position coordinates
-  const pickedEntities = this._viewer.scene.drillPick(mousePos)
-  // if there are entities found
-  if (pickedEntities.length) {
-    // extract and store the picked entities only
-    this.pickedList = this.pickedList.concat(pickedEntities.map(x => x.id))
+    // look for any enties at the provided position coordinates
+    const pickedEntities = this._viewer.scene.drillPick(mousePos)
+    // if there are entities found
+    if (pickedEntities.length) {
+      // extract and store the picked entities only
+      this.pickedList = this.pickedList.concat(pickedEntities.map(x => x.id))
+    }
+    // if there items picked and found, raise the event and pass the 
+    // pickedList as a combination of: [{Entity}, {ImageryLayerFeatureInfo}]
+    return this.pickedList.length && this.onPicked.raiseEvent(this.pickedList)
   }
-  // if there items picked and found, raise the event and pass the 
-  // pickedList as a combination of: [{Entity}, {ImageryLayerFeatureInfo}]
-  return this.pickedList.length && this.onPicked.raiseEvent(this.pickedList)
+  // no matching positions or not clicking on map
+  return false
 }
 
 /**
@@ -95,7 +105,7 @@ async function mousePicker (mouse) {
  *
  * @return  {Array}                 The array of features.
  */
-async function getImageryLayerFeatures (position) {
+async function getImageryLayerFeatures(position) {
   // get the ray for lookup
   const pickRay = this._viewer.camera.getPickRay(position)
   // look for imagery layer features at provided ray
@@ -104,7 +114,7 @@ async function getImageryLayerFeatures (position) {
     this._viewer.scene
   )
   // filter any found features that only include feature info properties
-  const filteredFeatures = featuresPromise.filter( x => {
+  const filteredFeatures = featuresPromise.filter(x => {
     return x.properties && Object.getOwnPropertyNames(x.properties).length
   })
   // return the filtered features
@@ -118,7 +128,7 @@ async function getImageryLayerFeatures (position) {
  *
  * @return  {Cartesian3}            The found position on the ellipsoid.
  */
-function isCartesian (position) {
+function isCartesian(position) {
   return this._viewer.camera.pickEllipsoid(
     position,
     this._viewer.scene.globe.ellipsoid
